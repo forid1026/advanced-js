9e1fd838ee06a04efe2c701f89ce0c24f80266eb
forid1026 <sheikhforid1026@gmai.com>
2021-08-26 07:46:05 +0600

    added truely and falsy file

---
+++ true-false.js
@@ -0,0 +1,17 @@
+//falsy
+// 0
+// false
+// ''
+// null
+// NAN
+
+//truly
+//  '0' ' ' {} [] 'false'
+
+let  num = '';
+if(num){
+    console.log('Condition is true')
+}
+else{
+    console.log('Condition is false');
+}
